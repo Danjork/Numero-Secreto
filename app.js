b1af378a17b7e6a3b174149d0f665686f8c1a3d3
Danjork <40806603+Danjork@users.noreply.github.com>
2024-02-04 17:12:23 -0300

    funcion
    genera numero secreto

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-let titulo = document.querySelector('h1');
+/**let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Juego del numero secreto';
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = "Ingresa un numero del 1 al 10";
 
 /*oneclick trabaja con funciones 
-funciona = intentode usuario()
+
 */
+let numeroSecreto = generaNumeroSecreto();
+console.log(numeroSecreto);
 
-function intentoDeUsuario(){
-    alert('click desde el boton');
 
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML= document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 };
+
+function generaNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+  
+}
+/* ejemplo elemento = h1 o P o h2 ; texto = "hola mundo"*/
+asignarTextoElemento('h1','Juego del numero secreto');
+asignarTextoElemento('p','Indica un número del 1 al 10');
+
