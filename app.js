5252468514330723715db604cc0f63cc09d9526e
Danjork <40806603+Danjork@users.noreply.github.com>
2024-02-04 18:28:31 -0300

    limpiar caja

--- app.js
+++ app.js
@@ -10,37 +10,56 @@ parrafo.innerHTML = "Ingresa un numero del 1 al 10";
 let numeroSecreto = generaNumeroSecreto();
 console.log(numeroSecreto);
 
+let intentos = 1;
 
-
-function asignarTextoElemento(elemento, texto){
-    let elementoHTML= document.querySelector(elemento);
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 };
 
 function generaNumeroSecreto() {
-    return Math.floor(Math.random()*10)+1;
-  
+    return Math.floor(Math.random() * 10) + 1;
+
 }
 
-    /*capturamos el valor que el usuario ingreso en el input*/
-   /* let numeroDeUsuario = document.querySelector('input');*/
-   /* */
-function verificaIntento(){
+/*capturamos el valor que el usuario ingreso en el input*/
+/* let numeroDeUsuario = document.querySelector('input');*/
+/* */
+function verificaIntento() {
 
-let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+console.log(intentos)
+    if (numeroDeUsuario === numeroSecreto) {
+        /*uso de operador ternario usar if else en una linea de codigo */
+        asignarTextoElemento('p', `acertaste el numero en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+    } else {
+        //el usuario no acerto
+        if(numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El numero secreto es menor');
+        } else{
 
-if(numeroDeUsuario === numeroSecreto){
-    asignarTextoElemento('p','acertaste el numero');
-}else{
-    asignarTextoElemento('p', 'para la proxima');
-}
-return;
+            asignarTextoElemento('p', 'El numero secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+
+    }
+    return;
+};
+
+function limpiarCaja(){
+    /*reutilizar el elemento del id del input
+    let valorCaja = document.querySelector('#valorUsuario');
+    valorCaja.value=''; */
+
+    //resumido el codigo
+    document.querySelector('#valorUsuario').value = '';
 };
 
 
 
 /* ejemplo elemento = h1 o P o h2 ; texto = "hola mundo"*/
-asignarTextoElemento('h1','Juego del numero secreto');
-asignarTextoElemento('p','Indica un número del 1 al 10');
+asignarTextoElemento('h1', 'Juego del numero secreto');
+asignarTextoElemento('p', 'Indica un número del 1 al 10');
 
