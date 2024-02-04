48808d9f1e81c23f1485901bf3b0fbb48eef7826
Danjork <40806603+Danjork@users.noreply.github.com>
2024-02-04 16:30:18 -0300

    agregando funcion

---
+++ app.js
@@ -0,0 +1,14 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Ingresa un numero del 1 al 10";
+
+/*oneclick trabaja con funciones 
+funciona = intentode usuario()
+*/
+
+function intentoDeUsuario(){
+    alert('click desde el boton');
+
+};
