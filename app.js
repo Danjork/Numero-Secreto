b8a4590ef530ee7956f900d74d614867482a0df0
Danjork <40806603+Danjork@users.noreply.github.com>
2024-02-04 17:33:07 -0300

    verifica numero

--- app.js
+++ app.js
@@ -22,6 +22,24 @@ function generaNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
   
 }
+
+    /*capturamos el valor que el usuario ingreso en el input*/
+   /* let numeroDeUsuario = document.querySelector('input');*/
+   /* */
+function verificaIntento(){
+
+let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+if(numeroDeUsuario === numeroSecreto){
+    asignarTextoElemento('p','acertaste el numero');
+}else{
+    asignarTextoElemento('p', 'para la proxima');
+}
+return;
+};
+
+
+
 /* ejemplo elemento = h1 o P o h2 ; texto = "hola mundo"*/
 asignarTextoElemento('h1','Juego del numero secreto');
 asignarTextoElemento('p','Indica un número del 1 al 10');
